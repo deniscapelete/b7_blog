3569a09a48eb7016ea15b1ebbb95e24955bef417
Denis Capelete <capelete.denis@gmail.com>
2025-09-20 19:31:25 -0300

    Relacionamento Post-User nos Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User;
 
 class Post extends Model
 {
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'authorId');
+    }
 }
