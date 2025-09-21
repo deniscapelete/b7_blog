038b41f76dc39aeaada2cf65a2014c3608cb7717
Denis Capelete <capelete.denis@gmail.com>
2025-09-21 09:56:18 -0300

    Relacionamentos Post - Tag no banco

---
+++ Untitled-1.md
@@ -0,0 +1,28 @@
+
+
+# BLOG
+-- Resources
+## Post
+- id
+- Slug
+- Author (user_id)
+- Title
+- Content
+- Cover (image)
+- CreatedAt
+- UpdatedAt
+- Status (DRAFT, PUBLISHED, ARCHIVED)
+
+## User
+- id
+- name
+- email
+- password
+- status
+## Tag
+- id
+- name
+
+##post_tags
+- postId
+- tagId
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User;
 
 class Post extends Model
@@ -23,5 +24,10 @@ class Post extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'authorId');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag', 'postId', 'tagId');
     }
 }
